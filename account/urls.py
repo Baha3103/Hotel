e853d9d6c4bd2ca81a3fa7df9d112590de49d6e5
Baha <ggwpbaha@gmail.com>
2022-02-24 01:30:18 +0600

    nerf

--- account/urls.py
+++ account/urls.py
@@ -1,18 +1,3 @@
-# from django.urls import path
-#
-# from account.views import RegistrationView, activate, LoginView, LogoutView, ChangePasswordView, \
-#     ForgotPasswordView, ForgotPasswordComlete
-#
-# urlpatterns = [
-#     path('register/', RegistrationView.as_view()),
-#     path('activate/<str:uid>/<str:token>/', activate, name="activate"),
-#     # path('activate/<str:activation_code>/', ActivateView.as_view()),
-#     path('login/', LoginView.as_view()),
-#     path('logout/', LogoutView.as_view()),
-#     path('change_password/', ChangePasswordView.as_view()),
-#     path('forgot_password/', ForgotPasswordView.as_view()),
-#     path('forgot_password_complete/', ForgotPasswordComlete.as_view())
-# ]
 
 
 from django.urls import path
